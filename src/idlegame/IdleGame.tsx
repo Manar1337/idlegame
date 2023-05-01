e88fd152799c02bee163d695e1d03ae17a6aa4ea
Llinl <manar@comhem.se>
2023-05-01 20:49:57 +0200

    made the first folder

---
+++ src/idlegame/IdleGame.tsx
@@ -0,0 +1,12 @@
+import { Box, Typography } from "@mui/material";
+
+interface IdleGameProps {
+}
+
+function IdleGame(props: IdleGameProps) {
+    const { } = props;
+    return(
+        <Box><Typography variant="h3">Welcome to my idlegame</Typography></Box>
+    )
+};
+export default IdleGame;
